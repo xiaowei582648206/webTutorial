2719d1d4de097cdb581cdfe49152c0b437e94c52
xiaowei <xiaowei582648206@163.com>
2019-03-11 17:25:03 +0800

    通过修改数据库连接字符串、增加jdbc.properties配置文件，实现数据库本地化适配

--- src/com/hnu/cloudmining/dbc/DatabaseConnection.java
+++ src/com/hnu/cloudmining/dbc/DatabaseConnection.java
@@ -1,21 +1,30 @@
 package com.hnu.cloudmining.dbc;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by Administrator on 2019/3/8.
  */
 public class DatabaseConnection {
-    private static final String dirver = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://192.168.122.128:3306/cloudmining";
-    private static final String user = "root";
-    private static final String password = "123";
+    private static  String driver ;
+    private static  String url = "jdbc:mysql://192.168.122.128:3306/cloudmining?useUnicode=true&characterEncoding=utf-8 ";
+    private static  String user = "root";
+    private static  String password = "123";
     private Connection conn = null;
 
-    public DatabaseConnection() throws ClassNotFoundException, SQLException {
-        Class.forName(dirver);
+    public DatabaseConnection() throws ClassNotFoundException, SQLException, IOException {
+        Properties p = new Properties();
+       InputStream in =  this.getClass().getClassLoader().getResourceAsStream("conf/jdbc.properties");
+        p.load(in);
+        driver = p.getProperty("driver");
+        url = p.getProperty("url");
+        user = p.getProperty("user");
+        password = p.getProperty("password");
+        Class.forName(driver);
         conn = DriverManager.getConnection(url, user, password);
     }
 
