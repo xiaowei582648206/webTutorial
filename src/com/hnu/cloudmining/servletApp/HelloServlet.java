808380d58a07b5d7affde8e58e91fcb2af91ff10
xiaowei <xiaowei582648206@163.com>
2019-03-11 17:26:27 +0800

    编写javaServlet的demo

---
+++ src/com/hnu/cloudmining/servletApp/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.hnu.cloudmining.servletApp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Administrator on 2019/3/11.
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<head><title> π”√javaServlet±‡–¥hellowrold≥Ã–Ú</title></head>");
+        writer.println("<body>");
+        writer.println("<h1><font color=\"red\">hello world</font></h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+        writer.close();
+    }
+}
