2f8b6ae450f11e45b7156dd67a5dce4da44e4bae
xiaowei <xiaowei582648206@163.com>
2019-03-13 17:59:15 +0800

    总结过滤器的心得体会

--- src/com/hnu/cloudmining/servletApp/LoginFilter.java
+++ src/com/hnu/cloudmining/servletApp/LoginFilter.java
@@ -24,7 +24,7 @@ public class LoginFilter implements Filter {
             filterChain.doFilter(servletRequest,servletResponse); // 自动判断过滤链中是否还有在web.xml中定义的过滤器还没有执行，最后才将request请求发送给目标
         }else{  //如果session中没有userid属性，则表示当前用户没有登录，需要重新登录
             System.out.println("当前用户没有登录，请重新登录!");
-            request.getRequestDispatcher("login.jsp").forward(servletRequest,servletResponse); //服务器跳转，将本次请求通过requestDispather请求转发器(在服务器上)，转发给login.jsp页面，此时所有的操作都是在同一个request中完成的，也都是在服务器上完成的，由于只有一个request，所以过滤器也只会作用一次。
+            request.getRequestDispatcher("login.jsp").forward(servletRequest,servletResponse); //服务器跳转，将本次请求通过requestDispather请求转发器(服务器上)，转发给login.jsp，转而由login.jsp来处理本次request请求，此时所有的操作都是在同一个request中完成的，也都是在服务器上完成的，由于只有一个request，所以过滤器也只会作用一次。
 //            response.sendRedirect("login.jsp"); //客户端跳转，重定向，两次请求，response.sendRedirect(),将响应头传给浏览器，让浏览器立刻重新发一次请求，请求login.jsp页面，而这个请求又会被过滤器拦截，然后浏览器再发第三次请求，再被拦截，再发……如此循环下去，始终无法访问
         }
     }
